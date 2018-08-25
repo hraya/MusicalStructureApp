81686391baec0c8ff53663b1034cc91213e43104
hraya <hraya@lssu.edu>
2018-08-25 00:11:36 -0500

    Corrected Java classes and created activities

--- app/src/main/java/com/example/android/musicalstructureapp/ArtistActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/ArtistActivity.java
@@ -1,4 +1,13 @@
 package com.example.android.musicalstructureapp;
 
-public class ArtistActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArtistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.music_list);
+    }
 }
--- app/src/main/java/com/example/android/musicalstructureapp/PlaylistActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/PlaylistActivity.java
@@ -1,4 +1,13 @@
 package com.example.android.musicalstructureapp;
 
-public class PlaylistActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaylistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+    }
 }
